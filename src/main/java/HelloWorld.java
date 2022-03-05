d7fece2677b24b708634419a6678e91b62664b42
Henrique Pereira <riqueferreira55@gmail.com>
2022-03-05 22:01:14 +0000

    comit inicial

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HellowWorld extends JFrame {
+    private JPanel panel1;
+    public HellowWorld(){
+        setContentPane(panel1);
+        pack();
+    }
+    public static void main(String[] args) {
+        new HellowWorld().setVisible(true);
+    }
+}
